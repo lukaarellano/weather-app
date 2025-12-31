cd7eb64438fa2b8fe4eab0b5ee3b3ae1d7fd8af5
lukaarellano <arellanoluka76+git@gmail.com>
2025-12-31 16:18:44 -0600

    Added a location entry to the GUI (doesn't work

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ import requests
 import json
 from tkinter import * #type: ignore
 
-response = requests.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Houston/?key=TDYFEERMRS7Y6F5CLGSDLET5R&include=current")
+display = Tk()
 
-print(response.status_code)
-
-weatherDict = response.json()
-temperature = weatherDict['days'][0]["temp"]
-datetime = weatherDict['days'][0]["datetime"]
-conditions = weatherDict['days'][0]['conditions']
-temphigh = weatherDict['days'][0]['tempmax']
-tempmin = weatherDict['days'][0]['tempmin']
+display.title("Weather App 🌧️")
+display.geometry("500x250")
 
 def update_weather():
-    with open("weather-app/data/data.json", "r+") as json_file:
+    def update_status():
+        status.config(text=status_text)
+    def update_location():
+        global response
+        global location_var
+        global weatherDict
+        location_var.set(location_var.get())
+        print(location_var.get())
+        response = requests.get(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location_var.get()}/?key=TDYFEERMRS7Y6F5CLGSDLET5R&include=current")
+        weatherDict = response.json()
+    with open("data/data.json", "r+") as json_file:
+        update_location()
         data = json.load(json_file)
+        data['location'] = location_var.get()
         data["temperature"] = temperature
         data["datetime"] = datetime
+        data['time'] = time
         data["conditions"] = conditions
         data["temphigh"] = temphigh
         data["tempmin"] = tempmin
@@ -28,26 +35,38 @@ def update_weather():
         
         update_status()
 
-display = Tk()
+location_var = StringVar()
+location_var.set("Houston")
+location_label = Label(display, text='Location:')
+location_entry = Entry(display, textvariable=location_var)
 
-display.title("Weather App 🌧️")
-display.geometry("500x150")
+response = requests.get(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location_var.get()}/?key=TDYFEERMRS7Y6F5CLGSDLET5R&include=current")
+
+print(response.status_code)
+
+weatherDict = response.json()
+temperature = weatherDict['currentConditions']["temp"]
+datetime = weatherDict['days'][0]["datetime"]
+time = weatherDict['currentConditions']['datetime']
+conditions = weatherDict['days'][0]['conditions']
+temphigh = weatherDict['days'][0]['tempmax']
+tempmin = weatherDict['days'][0]['tempmin']
 
 status_text = f"""
+Location: {location_var.get()}
 Date: {datetime}
+Time: {time}
 Temperature: {temperature}
 Conditions: {conditions}
 Highest Temperature: {temphigh}
-Lowest Temperature: {tempmin}
-"""
+Lowest Temperature: {tempmin}"""
 status = Label(display, text=status_text)
 status.pack()
 
+location_label.pack()
+location_entry.pack()
+
 update_stats = Button(display, text='Update Weather', command=update_weather)
 update_stats.pack()
-
-def update_status():
-    status.config(text=status_text)
-    print(status_text)
 
 display.mainloop()
